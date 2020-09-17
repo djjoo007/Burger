d7143909812bca7af5dabb67d9afd4843a90c982
Joo Heredia <djjoo007uw@gmail.com>
2020-09-17 11:12:51 -0700

    add burgers_controller.js

---
+++ controllers/burgers_controllers.js
@@ -0,0 +1,61 @@
+const express = require ('express');
+
+const router = express.Router();
+
+const burger = require('../models/burger.js');
+
+//Get Route
+router.get('/', function(req, res) {
+    burger.all(function(data) {
+        const hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render('index', hbsObject);
+    });
+});
+
+//Post Route
+router.post('/api/burger', function(req, res) {
+    burger.create([
+        'burger_name', 'devoured'
+    ], [
+        req.body.burger_name, req.body.devoured
+    ], function(result) {
+        res.json({ id: result.insertId });
+    });
+});
+
+//Put Route
+router.put('/api/burgers/:id', function(req,res) {
+    const condition = 'id = ' + req.params.id;
+
+    console.log('condition', condition);
+
+    burger.update({
+        devoured: req.body.devoured
+    }, condition, function(result) {
+        if (result.changeRows == 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+
+//Delete Route
+router.delete('/api/burgers/:id', function(req, res) {
+    const condition = "id = " + req.params.id;
+
+    burger.delete(condition, function(result) {
+        if (result.affectedRows == 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+
+
+// Export routes
+module.export = router;
