a510e611c908fc09825ff9e23946be8b6b626202
Joo Heredia <djjoo007uw@gmail.com>
2020-09-17 09:34:56 -0700

    add seeds.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,12 @@
+DROP DATABASE IF EXISTS burger_db;
+
 CREATE DATABASE burger_db;
+
 USE burger_db;
 
 CREATE TABLE burgers (
-    id int NOT NULL AUTO_INCREMENT,
-    burger_name varchar(255) NOT NULL,
-	devoured BOOLEAN DEFAULT false,
+    id INT NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR (100) NOT NULL,
+	devoured BOOLEAN NOT NULL,
 	PRIMARY KEY (id)
 )
